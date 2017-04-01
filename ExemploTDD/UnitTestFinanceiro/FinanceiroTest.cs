df54aa89fea37ca5e96068661fc982b4ea5b02e7
eli <elipedrodos.santos@gmail.com>
2017-04-01 20:22:29 -0300

    OK

--- ExemploTDD/UnitTestFinanceiro/FinanceiroTest.cs
+++ ExemploTDD/UnitTestFinanceiro/FinanceiroTest.cs
@@ -1,14 +1,24 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ExemploTDD.Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UnitTestFinanceiro
 {
     [TestClass]
     public class FinanceiroTest
     {
+        [TestMethod]
+        public void ValorImpostoTeste()
+        {
+            var financeiro = new Financeiro();
+            var valor = financeiro.ValorImposto(20, "SP");
+
+            Console.WriteLine("Estado: SP");
+            Console.WriteLine("Valor da venda : 100,00");
+            Console.WriteLine("Valor esperado : 10,00");
+            Console.WriteLine("Valor retornado: {0}", valor);
+            
+            Assert.IsTrue(valor == 10, "O valor deve ser 10 se a venda 100 e o estado é SP");
+        }
     }
 }
